fc3b244574ea510f019194ae9a61faeabf34620f
craigwann <craigwann@me.com>
2018-07-20 10:49:44 -0700

    add ERB's layout, index, brands and stores

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem("sinatra-activerecord")
 gem("rake")
 gem("pg")
 gem("sinatra")
+gem 'thin'
+
 
 group(:test) do
   gem("rspec")
--- app.rb
+++ app.rb
@@ -12,3 +12,21 @@ get("/brands") do
   @brands = Brand.all()
   erb(:brands)
 end
+
+get("/stores") do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post("/brands") do
+  name = params.fetch("brand_name")
+  price = params.fetch("brand_price")
+  Brand.create({:name => name, :price => price})
+  redirect("/brands")
+end
+
+post("/stores") do
+  name = params.fetch("store_name")
+  Store.create({:name => name})
+  redirect("/stores")
+end
