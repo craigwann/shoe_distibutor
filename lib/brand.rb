8a1bd85c9407934a587aa75dae1810cec9e9cd92
craigwann <craigwann@me.com>
2018-07-20 08:49:48 -0700

    update test database, pass brand rspec

---
+++ lib/brand.rb
@@ -0,0 +1,13 @@
+class Brand < ActiveRecord::Base
+  has_and_belongs_to_many(:stores)
+  validates(:name, :presence => true)
+  validates :name, uniqueness: true
+  before_save(:titlecase_name)
+
+
+  private
+
+  def titlecase_name
+    self.name = name.split.map(&:capitalize).join(' ')
+  end
+end
---
+++ lib/store.rb
@@ -0,0 +1,13 @@
+class Store < ActiveRecord::Base
+  has_and_belongs_to_many(:brands)
+  validates(:name, :presence => true)
+  validates :name, uniqueness: true
+  before_save(:titlecase_name)
+
+
+  private
+
+  def titlecase_name
+    self.name = name.split.map(&:capitalize).join(' ')
+  end
+end
