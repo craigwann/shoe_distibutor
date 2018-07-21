c26b78782f4343631841f7c002d38bcc3e39fa47
craigwann <craigwann@me.com>
2018-07-21 15:15:26 -0700

    add price to brand select input

--- app.rb
+++ app.rb
@@ -33,11 +33,6 @@ end
 
 get("/brands/:id") do
   @brand = Brand.find(params.fetch("id").to_i())
-  # if @brand.store_id
-  #   @store = Store.find(@brand.store_id)
-  # else
-  #   @store = nil
-  # end
   @stores = Store.all
   erb(:brand)
 end
